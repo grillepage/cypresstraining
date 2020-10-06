2ec5545d0c08fc8c8b6513917a55de8208868b43
Alexander Bondarenko <grillepage@gmail.com>
2020-10-06 14:29:23 +0300

    Lesson 11

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -40,7 +40,7 @@ describe('Our first suit', () => {
         cy.get('[data-cy="imputEmail1"]')
     })
     
-    it.only('second test', () => {
+    it('second test', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -63,5 +63,47 @@ describe('Our first suit', () => {
         .find('[type="email"]')
     })
 
+
+    it.only('then and wrap methods', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // cy.contains('nb-card', 'Using the Grid')
+        // .find('[for="inputEmail1"]')
+        // .should('contain', 'Email')
+
+        // cy.contains('nb-card', 'Using the Grid')
+        // .find('[for="inputPassword2"]')
+        // .should('contain', 'Password')
+
+        // cy.contains('nb-card', 'Basic form')
+        // .find('[for="exampleInputEmail1"]')
+        // .should('contain', 'Email address')
+
+        // cy.contains('nb-card', 'Basic form')
+        // .find('[for="exampleInputPassword1"]')
+        // .should('contain', 'Password')
+        
+        //Cypress style
+        cy.contains('nb-card', 'Using the Grid')
+        .then( firstForm => {
+            const emailLabelFirst = firstForm
+            .find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm
+            .find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then( secondForm => {
+                const passwordLabelSecond = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst).to.equal(passwordLabelSecond)
+            
+                //Как переключиться обратно с jquary на cypress формат тестов
+                cy.wrap(secondForm)
+                .find('[for="exampleInputPassword1"]').should('contain', 'Password')
+            })   
+        })
+    })
 })
 
